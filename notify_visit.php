74332aee35693f91f0021b5c994df1082ee5018c
HSun981 <heng.sun@vanderbilt.edu>
2022-01-12 18:22:59 -0600

    test a supplement to send email when a specific link is visited

---
+++ notify_visit.php
@@ -0,0 +1,12 @@
+<html>
+ <head>
+  <title>Save the RAs</title>
+ </head>
+ <body>
+ <?php echo '<p>Thank you for scanning the code. The RAs will be notified of your inputs in the mailbox,
+    and no personal information of yours will be collected.</p>';
+ mail('heng.sun@vanderbilt.edu','RA Mailbox', 'Someone indicated something is put in the RA
+ Mailbox.')
+ ?>
+ </body>
+</html>
